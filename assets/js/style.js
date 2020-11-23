a162c3f766bd053f654db1d78fe685362d69d2af
Dave <bbec0689@gmail.com>
2020-11-22 21:03:26 -0700

    added a feature to automatically direct you to the page to enter your name and view your score

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,19 +1,16 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-const scoreDiv = document.getElementById("scoreContainer");
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-var timerEl = document.getElementById('countdown')
+var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
 
-var playerScore = 100;
-var highscore = localStorage.getItem("highscore");
-
-let shuffledQuestions, currentQuestionIndex
-
+let shuffledQuestions, currentQuestionIndex;
+let = numCorrect = 5;
 
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
@@ -26,63 +23,70 @@ function startGame() {
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
     setNextQuestion();
 }
 
 function setNextQuestion() {
     resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
+
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
     question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
         if (answer.correct) {
-            button.dataset.correct = answer.correct
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
     })
 }
 
 function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
+    clearStatusClass(document.body);
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
 function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
     Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
+        setStatusClass(button, button.dataset.correct);
     })
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide')
+        currentQuestionIndex++;
+        setNextQuestion();
     } else {
-        startButton.innerText = 'Restart'
-        startButton.classList.remove('hide')
+        startButton.innerText = 'Submit';
+        startButton.classList.remove('hide');
+        window.location.href = "./secondary.html";
     }
 }
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
+    clearStatusClass(element);
     if (correct) {
-        element.classList.add('correct')
+        element.classList.add('correct');
+
     } else {
-        element.classList.add('wrong')
+        element.classList.add('wrong');
+        
     }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 // Questions and answers 
 const questions = [
@@ -122,24 +126,29 @@ const questions = [
 ]
 
 function countdown() {
-    var timeLeft = 2;
+    var timeLeft = 100;
 
     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
             timerEl.innerText = timeLeft;
-            timeLeft = timeLeft - 1
+            timeLeft = timeLeft - 1;
         }
         else if (timeLeft === 0) {
             clearInterval;
             timerEl.innerText = "";
-            displayForm()
+            displayForm();
         }
     }, 1000);
 }
 
 function displayForm() {
-    window.location.href = "./secondary.html"
+    window.location.href = "./secondary.html";
 }
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+};
 
 startButton.onclick = countdown;
